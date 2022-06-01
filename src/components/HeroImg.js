e0935e2fc0320dd8fbd54eae1a06a3ab162af7e9
Well-199 <wellington.marunaka@gmail.com>
2022-05-31 21:03:31 -0300

    0.0.1

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -7,14 +7,14 @@ const HeroImg = () => {
     return(
         <div className='hero'>
             <div className='mask'>
-                <img className='into-img' src={IntroImg} alt="IntroImg"/>
+                <img className='intro-img' src={IntroImg} alt="IntroImg"/>
             </div>
             <div className='content'>
                 <p>HI, I'M A FREELANCER.</p>
                 <h1>React Developer.</h1>
                 <div>
-                    <Link to="/customers" className="btn">Clientes</Link>
-                    <Link to="/contact" className="btn btn-light">Contato</Link>
+                    <Link to="/customers" className="btn">Customers</Link>
+                    <Link to="/contact" className="btn btn-light">Contact</Link>
                 </div>
             </div>
         </div>
